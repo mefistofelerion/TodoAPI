9613e381de05d27e9adcab9884a83da5a425237e
Ivan Guerra <ivan.aguerra616@gmail.com>
2016-08-28 12:22:31 -0500

    added POST for todos

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Do iOS course',
-	completed: false
-	},
-	{
-	id: 2,
-	description: 'Complete Angular course!',
-	completed: false
-	},
-	{
-		id: 3,
-		description: 'Complete everything',
-		completed: true
-	}
-];
+var todoNextId = 1;
+var todos = [];
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res){
 	res.send('Todo API root');
@@ -45,6 +37,19 @@ app.get('/todo/:id', function(req, res){
 	}
 });
 
+app.post('/todos', function(req, res){
+	var body = req.body;
+
+	if(typeof body !== 'undefined'){
+		body.id = todoNextId++;
+		todos.push(body);
+		res.json('Todo task added');
+	}else{
+		res.send('empty json sent, nothing was saved');
+	}
+	
+});
+
 app.listen	(PORT, function(){
 	console.log('Server started on port ' + PORT);
-})
+});
