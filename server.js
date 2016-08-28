caaceee44c3b547218d02bb5556904c7629aaa30
Ivan Guerra <ivan.aguerra616@gmail.com>
2016-08-28 16:02:46 -0500

    added filter for ?q

--- server.js
+++ server.js
@@ -24,8 +24,12 @@ app.get('/todos', function(req, res){
 		filteredTodos = _.where(todos, {completed: true});
 	}else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false'){
 		filteredTodos = _.where(todos,{completed: false});
-	}else{
-		filteredTodos = todos;
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0){
+		filteredTodos = _.filter(filteredTodos, function(todo){
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) === -1 ?  false : true;
+		});
 	}
 
 	res.json(filteredTodos);
