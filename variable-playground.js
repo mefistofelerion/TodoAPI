71aaf89e921af3a62427f621d2a0d966f3b1a8ac
Ivan Guerra <ivan.aguerra616@gmail.com>
2016-08-28 15:24:43 -0500

    added variable-playground area

---
+++ variable-playground.js
@@ -0,0 +1,24 @@
+// var person = {
+// 	name: 'Ivan',
+// 	age:21
+// };
+
+// function updatePerson(obj){
+// 	obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+//Array example
+
+var grades = [15,88];
+
+function addGrades(grad){
+	// grades.push(55);
+	grad = [12,33,99];
+	debugger;
+}
+
+addGrades(grades);
+console.log(grades);
