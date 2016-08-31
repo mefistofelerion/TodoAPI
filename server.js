6cbd54ae751a2d32ec7a179ed7a52bb372da3aee
Ivan Guerra <ivan.aguerra616@gmail.com>
2016-08-30 20:08:01 -0500

    removed commented lines, and configured db for heroku production environment

--- server.js
+++ server.js
@@ -39,25 +39,6 @@ app.get('/todos', function(req, res) {
 	}, function(error){
 		res.status('500').send();
 	});
-	// var filteredTodos = todos;
-
-	// if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(todos, {
-	// 		completed: true
-	// 	});
-	// } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(todos, {
-	// 		completed: false
-	// 	});
-	// }
-
-	// if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) === -1 ? false : true;
-	// 	});
-	// }
-
-	// res.json(filteredTodos);
 });
 
 app.get('/todo/:id', function(req, res) {
